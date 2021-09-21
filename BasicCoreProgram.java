3e4f6e4b06f4a82397205070b8c7d89905094c3a
unknown <rajputnikita5666@gmail.com>
2021-09-21 22:46:15 +0530

    Adding welcome message

---
+++ BasicCoreProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.basiccore;
+
+public class BasicCoreProgram {
+
+	public static void main(String[] args) {
+		//Welcome 
+		System.out.println("Welcome To Basic Core Program");
+	}
+}
